e43ef9e4b6130f2c5cd6da75070879fe3a18bad5
Alexander Choi <168526775+alicksc@users.noreply.github.com>
2025-09-30 21:09:00 -0700

    Complete 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let temp = "";
 
+    if(num < 0) {
+        return "ERROR";
+    }
+    
+    for(let i = 0; i < num; i++) {
+        temp += str;
+    }
+
+    return temp;
 };
 
 // Do not edit below this line
