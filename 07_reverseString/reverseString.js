0d603ef57319a71c04ddab2c6f83a5faa748feff
Alexander Choi <168526775+alicksc@users.noreply.github.com>
2025-09-30 21:24:21 -0700

    Complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let temp = "";
+    
+    for(let i = str.length - 1; i >= 0; i--) {
+        temp += str[i];
+    }
 
+    return temp;
 };
 
 // Do not edit below this line
